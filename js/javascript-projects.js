53571a0b3fbcb75bbf81217ced48e5b0349542c3
Kresqle <75183858+Kresqle@users.noreply.github.com>
2021-04-30 21:30:38 +0200

    Add files via upload

---
+++ js/javascript-projects.js
@@ -0,0 +1,5 @@
+const randomTextGenerator = document.getElementById("randomTextGenerator");
+
+randomTextGenerator.addEventListener("click", () => {
+  document.location.href = "../html/random-text-generator.html";
+});
