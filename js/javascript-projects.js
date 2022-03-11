acc71630790d13f0399a360fc89df870791a54fa
Kresqle <75183858+Kresqle@users.noreply.github.com>
2022-03-11 08:58:58 +0100

    Delete js directory

--- js/javascript-projects.js
+++
@@ -1,5 +0,0 @@
-const randomTextGenerator = document.getElementById("randomTextGenerator");
-
-randomTextGenerator.addEventListener("click", () => {
-  document.location.href = "../html/random-text-generator.html";
-});
